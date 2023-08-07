dbe8595a24909f25581708e131f6f7f5871eca07
Maria Medeiros <maitevelori@hotmail.com>
2023-08-07 16:33:52 -0700

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -8,3 +8,21 @@ const slidesLength = slideRight.querySelectorAll("div").length;
 let activeSlideIndex = 0;
 
 slideLEft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+upButton.addEventListener("click", () => changeSlide("up"));
+
+downButton.addEventListener("click", () => changeSlide("down"));
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
